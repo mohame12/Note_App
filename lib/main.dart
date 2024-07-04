3168850a237dab6093f61718aae700d89a35bff8
MohamedFarag <71080118+mohame12@users.noreply.github.com>
2024-07-04 21:03:25 +0300

    addMultiBlocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/Cubits/AddNoteCubit/add_note_cubit.dart';
 import 'package:note_app/consts.dart';
 import 'package:note_app/models/note_model.dart';
 import 'package:note_app/views/edit_view.dart';
@@ -24,17 +26,23 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-  return MaterialApp(
-    debugShowCheckedModeBanner: false,
+  return MultiBlocProvider(
+    providers: [
+      BlocProvider(create: (context) => AddNoteCubit(),),
 
-    theme: ThemeData(
-        brightness: Brightness.dark,
-    fontFamily: 'Poppins'),
-    routes: {
-      NoteView.id:(context)=> const NoteView(),
-      EditView.id:(context)=>const EditView(),
-    },
-    initialRoute: NoteView.id,
+    ],
+    child: MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      theme: ThemeData(
+          brightness: Brightness.dark,
+      fontFamily: 'Poppins'),
+      routes: {
+        NoteView.id:(context)=> const NoteView(),
+        EditView.id:(context)=>const EditView(),
+      },
+      initialRoute: NoteView.id,
+    ),
   );
   }
 }
